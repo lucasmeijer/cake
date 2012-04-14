e45502b48e181538a33491f8bd7c4b30e5f05edd
Lucas Meijer <lucas@unity3d.com>
2012-04-14 22:50:21 +0200

    include scanner tests.   copied regex from jamplus.

--- IncludeScanner.cs
+++ IncludeScanner.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace bs
 {
 	internal class IncludeScanner
 	{
+		Regex _regex = new Regex("^[ 	]*#[ 	]*include[ 	]*[<\"]([^\">]*)[\">].*$", RegexOptions.Multiline);
+
 		public IEnumerable<string> Scan(string file)
 		{
 			yield break;
+		}
+
+		public IEnumerable<string> ScanText(string fileContents)
+		{
+			var matches = _regex.Matches(fileContents);
+			return from Match match in matches select match.Groups[1].ToString();
 		}
 	}
 }
---
+++ Tests/IncludeScannerTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace bs.Tests
+{
+	[TestFixture]
+	public class IncludeScannerTest
+	{
+		[Test]
+		public void CanFindSimpleInclude()
+		{
+			RunTest("#include \"windows.h\"", new [] {"windows.h"});
+		}
+
+		[Test]
+		public void CanFindIncludeWithSquareBrackets()
+		{
+			RunTest("#include <windows.h>", new[] { "windows.h" });
+		}
+
+		[Test]
+		public void CanFindMultipleIncludes()
+		{
+			RunTest("#include <windows.h>\n#include \"myfolder/myfile.h\"", new[] { "windows.h", "myfolder/myfile.h" });
+		}
+
+		private static void RunTest(string fileContents, IEnumerable<string> expected)
+		{
+			var scanner = new IncludeScanner();
+			var result = scanner.ScanText(fileContents).ToList();
+			CollectionAssert.AreEquivalent(expected, result);
+		}
+	}
+}
